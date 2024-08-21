ea733febdbabb15c38a42a436f5df8961362ce6f
zuokun <zuokun@gmail.com>
2024-08-21 11:42:56 -0700

    added booked 4 srt to md

--- vse/4/printsrt2.py
+++ vse/4/printsrt2.py
@@ -1,8 +1,9 @@
 import re
+import os
 
-def main():
+def procfile(filepath):
     # read file line by line
-    file = open( "/Users/jzhang/git-repos/hdclass/vse/4/02-1 慧灯禅修课24 加行的修法-菩提心1.srt", "r")
+    file = open(filepath, "r", encoding='UTF-8')
     lines = file.readlines()
     file.close()
 
@@ -11,6 +12,29 @@ def main():
         if re.search('^[0-9]+$', line) is None and re.search('^[0-9]{2}:[0-9]{2}:[0-9]{2}', line) is None and re.search('^$', line) is None:
             text += ' ' + line.rstrip('\n')
         text = text.lstrip()
-    print(text)
+    # print(text)
 
-main()
+    f = open(filepath + ".md", "w", encoding='UTF-8')
+    f.write(text)
+    f.close()
+
+def procdir():
+    with os.scandir('.') as it:
+        for entry in it:
+            if entry.name.endswith(".srt") and entry.is_file():
+                print(entry.name, entry.path)
+                procfile(entry.path)
+
+
+    # directory = os.fsencode('.')
+        
+    # for file in os.listdir(directory):
+    #     filename = os.fsdecode(file)
+    #     if filename.endswith(".srt"): # or filename.endswith(".py"): 
+    #         print(os.path.join(directory, filename))
+    #         procfile(os.path.join(directory, filename))
+    #         continue
+    #     else:
+    #         continue
+
+procdir()
