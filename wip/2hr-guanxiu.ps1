c17869246c00b9e016d52acdbacf85f322a1e26a
zuokun <zuokun@gmail.com>
2024-09-18 18:30:31 -0700

    ygbx 6 wip

---
+++ wip/2hr-guanxiu.ps1
@@ -0,0 +1,4 @@
+mpv --start=0 --end=00:04 "https://s3.ap-northeast-1.wasabisys.com/hdcx/hdv/v/4jx/%E6%9A%87%E6%BB%A1%E9%9A%BE%E5%BE%97-%E4%B8%8A%E5%B8%88%E5%BF%B5%E8%AF%B5.mp4"
+ping -n 5 google.com
+
+mpv --start=00:04 --end=00:10 "https://s3.ap-northeast-1.wasabisys.com/hdcx/hdv/v/4jx/%E6%9A%87%E6%BB%A1%E9%9A%BE%E5%BE%97-%E4%B8%8A%E5%B8%88%E5%BF%B5%E8%AF%B5.mp4"
